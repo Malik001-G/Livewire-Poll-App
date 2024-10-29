f1c0a56f7df3b02e5269450265acda705f9e76fa
Malik001-G <adisamalikng0@gmail.com>
2024-10-29 16:12:44 +0100

    Create poll and options

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,8 +1,25 @@
 <div>
-    <form>
+    <form wire:submit.prevent ="createPoll">
         <label> Poll Title</label>
         <input type="text" wire:model.live="title" />
 
-        Current title: {{$title}}
+        <div class="mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add option</button>
+        </div>
+
+        <div class="mt-4">
+            @foreach ($options as $index => $option)
+            <div class="mb-4">
+                <label>Option {{$index + 1}}</label>
+                <div class="flex gap-2">
+                    <input type="text" wire:model.live="options.{{$index}}" />
+                    <button class="btn" wire:click="removeOption({{$index}})">Remove</button>
+                </div>
+            </div>
+
+            @endforeach
+        </div>
+
+        <button class="btn">Create Poll</button>
     </form>
 </div>
